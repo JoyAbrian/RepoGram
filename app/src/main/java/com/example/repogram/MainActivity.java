5bf85888505af48a7086a30ca1eada77572c2ace
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-01 02:15:22 +0800

    change scene toggle

--- app/src/main/java/com/example/repogram/MainActivity.java
+++ app/src/main/java/com/example/repogram/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.repogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ImageView activeFooterItems;
+    ImageView toggleHome;
+    ImageView toggleSearch;
+    ImageView toggleAdd;
+    ImageView toggleReels;
+    ImageView toggleProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         activeFooterItems = findViewById(R.id.toggleHome);
+        toggleHome = findViewById(R.id.toggleHome);
+        toggleSearch = findViewById(R.id.toggleSearch);
+        toggleAdd = findViewById(R.id.toggleAdd);
+        toggleReels = findViewById(R.id.toggleReels);
+        toggleProfile = findViewById(R.id.toggleProfile);
+
         activeFooterItems.setImageResource(R.drawable.footer_home_active);
+
+        toggleHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toggleSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/repogram/SearchActivity.java
+++ app/src/main/java/com/example/repogram/SearchActivity.java
@@ -2,11 +2,18 @@ package com.example.repogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class SearchActivity extends AppCompatActivity {
     ImageView activeFooterItems;
+    ImageView toggleHome;
+    ImageView toggleSearch;
+    ImageView toggleAdd;
+    ImageView toggleReels;
+    ImageView toggleProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,28 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         activeFooterItems = findViewById(R.id.toggleSearch);
+        toggleHome = findViewById(R.id.toggleHome);
+        toggleSearch = findViewById(R.id.toggleSearch);
+        toggleAdd = findViewById(R.id.toggleAdd);
+        toggleReels = findViewById(R.id.toggleReels);
+        toggleProfile = findViewById(R.id.toggleProfile);
+
         activeFooterItems.setImageResource(R.drawable.footer_search_active);
+
+        toggleHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toggleSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SearchActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
