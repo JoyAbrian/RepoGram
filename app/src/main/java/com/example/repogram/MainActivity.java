62c8511193d3d710c4eb93ba00e3ea80ba39223c
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-01 00:31:33 +0800

    home active on home activity

--- app/src/main/java/com/example/repogram/MainActivity.java
+++ app/src/main/java/com/example/repogram/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.repogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView activeFooterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activeFooterItems = findViewById(R.id.toggleHome);
+        activeFooterItems.setImageResource(R.drawable.footer_home_active);
     }
 }
