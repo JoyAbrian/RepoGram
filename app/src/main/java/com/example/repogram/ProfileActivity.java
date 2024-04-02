ba61ded10fc786702bda742ab8fea0593224acd8
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-03 01:01:16 +0800

    back to profile function

--- app/src/main/java/com/example/repogram/ProfileActivity.java
+++ app/src/main/java/com/example/repogram/ProfileActivity.java
@@ -108,6 +108,35 @@ public class ProfileActivity extends AppCompatActivity {
 
         if (username.getText().equals("joyrantepasang")) {
             activeFooterItems.setImageResource(R.drawable.footer_profile_active);
+        } else {
+            activeFooterItems.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String EXTRA_USERNAME = "joyrantepasang";
+                    Integer EXTRA_PICTURE = R.drawable.profile_anonym;
+                    String EXTRA_POST = "5";
+                    String EXTRA_FOLLOWERS = "537";
+                    String EXTRA_FOLLOWING = "530";
+                    Integer EXTRA_POST1 = R.drawable.profile_joyrantepasang_post1;
+                    Integer EXTRA_POST2 = R.drawable.profile_joyrantepasang_post2;
+                    Integer EXTRA_POST3 = R.drawable.profile_joyrantepasang_post3;
+                    Integer EXTRA_POST4 = R.drawable.profile_joyrantepasang_post4;
+                    Integer EXTRA_POST5 = R.drawable.profile_joyrantepasang_post5;
+
+                    Intent intent = new Intent(ProfileActivity.this, ProfileActivity.class);
+                    intent.putExtra("EXTRA_USERNAME", EXTRA_USERNAME);
+                    intent.putExtra("EXTRA_PICTURE", EXTRA_PICTURE);
+                    intent.putExtra("EXTRA_POST", EXTRA_POST);
+                    intent.putExtra("EXTRA_FOLLOWERS", EXTRA_FOLLOWERS);
+                    intent.putExtra("EXTRA_FOLLOWING", EXTRA_FOLLOWING);
+                    intent.putExtra("EXTRA_POST1", EXTRA_POST1);
+                    intent.putExtra("EXTRA_POST2", EXTRA_POST2);
+                    intent.putExtra("EXTRA_POST3", EXTRA_POST3);
+                    intent.putExtra("EXTRA_POST4", EXTRA_POST4);
+                    intent.putExtra("EXTRA_POST5", EXTRA_POST5);
+                    startActivity(intent);
+                }
+            });
         }
 
         toggleHome.setOnClickListener(new View.OnClickListener() {
