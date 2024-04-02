250e58b2c546af4ad1d1f708a423709b6593ea0d
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-02 21:37:33 +0800

    profile activity

---
+++ app/src/main/java/com/example/repogram/ProfileActivity.java
@@ -0,0 +1,48 @@
+package com.example.repogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+    ImageView activeFooterItems;
+    ImageView toggleHome;
+    ImageView toggleSearch;
+    ImageView toggleAdd;
+    ImageView toggleReels;
+    ImageView toggleProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        activeFooterItems = findViewById(R.id.toggleProfile);
+        toggleHome = findViewById(R.id.toggleHome);
+        toggleSearch = findViewById(R.id.toggleSearch);
+        toggleAdd = findViewById(R.id.toggleAdd);
+        toggleReels = findViewById(R.id.toggleReels);
+        toggleProfile = findViewById(R.id.toggleProfile);
+
+        activeFooterItems.setImageResource(R.drawable.footer_search_active);
+
+        toggleHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        toggleSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
