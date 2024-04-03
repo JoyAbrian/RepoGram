eb2d58cc227632319ba9a801cf722b3af02c25cd
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-03 22:25:01 +0800

    mas ammar

--- app/src/main/java/com/example/repogram/DataSource.java
+++ app/src/main/java/com/example/repogram/DataSource.java
@@ -12,7 +12,7 @@ public class DataSource {
         accounts.add(new Account("the.crusader.returns", "315", "31,2RB", "4466", R.drawable.profile_crusaderreturn_pp, R.drawable.profile_redmatthew2_post1, R.drawable.profile_redmatthew2_post2, R.drawable.profile_redmatthew2_post3, null, null, null, null, null, null, R.drawable.profile_redmatthew2_story));
         accounts.add(new Account("zvipey", "3", "53", "55", R.drawable.profile_zvipey_pp, R.drawable.profile_zvipey_post1, R.drawable.profile_zvipey_post2, null, null, null, null, null, null, null, R.drawable.profile_zvipey_story));
         accounts.add(new Account("bringmethehorizon", "982", "3,6JT", "11", R.drawable.profile_bmth_pp, R.drawable.profile_bmth_post1, R.drawable.profile_bmth_post2, R.drawable.profile_bmth_post3, null, null, null, null, null, null, R.drawable.profile_bmth_story));
-        accounts.add(new Account("bringmethehorizon", "982", "3,6JT", "11", R.drawable.profile_bmth_pp, R.drawable.profile_bmth_post1, R.drawable.profile_bmth_post2, R.drawable.profile_bmth_post3, null, null, null, null, null, null, R.drawable.profile_bmth_story));
+        accounts.add(new Account("ammartyo", "4", "1.932", "702", R.drawable.profile_ammar_pp, R.drawable.profile_ammar_post1, R.drawable.profile_ammar_post2, R.drawable.profile_ammar_post3, null, null, null, null, null, null, R.drawable.profile_ammar_story));
         return accounts;
     }
 }
