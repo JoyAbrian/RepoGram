6c62c4bd8e6fe41f20e0dd171901c630192ef48d
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-04-03 22:26:55 +0800

    mas fahri

--- app/src/main/java/com/example/repogram/DataSource.java
+++ app/src/main/java/com/example/repogram/DataSource.java
@@ -13,6 +13,7 @@ public class DataSource {
         accounts.add(new Account("zvipey", "3", "53", "55", R.drawable.profile_zvipey_pp, R.drawable.profile_zvipey_post1, R.drawable.profile_zvipey_post2, null, null, null, null, null, null, null, R.drawable.profile_zvipey_story));
         accounts.add(new Account("bringmethehorizon", "982", "3,6JT", "11", R.drawable.profile_bmth_pp, R.drawable.profile_bmth_post1, R.drawable.profile_bmth_post2, R.drawable.profile_bmth_post3, null, null, null, null, null, null, R.drawable.profile_bmth_story));
         accounts.add(new Account("ammartyo", "4", "1.932", "702", R.drawable.profile_ammar_pp, R.drawable.profile_ammar_post1, R.drawable.profile_ammar_post2, R.drawable.profile_ammar_post3, null, null, null, null, null, null, R.drawable.profile_ammar_story));
+        accounts.add(new Account("piscok.png", "24", "7", "5", R.drawable.profile_fahri_pp, R.drawable.profile_fahri_post1, null, null, null, null, null, null, null, null, R.drawable.profile_fahri_story));
         return accounts;
     }
 }
